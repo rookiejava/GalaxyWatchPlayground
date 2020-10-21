617e36ab34a4ccde38bd696c4baac871bf7154db
Kangho Hur <kangho.hur@samsung.com>
2020-10-21 16:56:41 +0900

    Adds the CustomFontDemo

---
+++ CustomFontDemo/AppShell.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Tizen.Wearable.CircularUI.Forms;
+
+namespace CustomFontDemo
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute("CustomFontPage", typeof(CustomFontPage));
+        }
+    }
+}
+
---
+++ CustomFontDemo/CustomFontPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: ExportFont("CuteFont-Regular.ttf")]
+[assembly: ExportFont("PTM55FT.ttf")]
+[assembly: ExportFont("Dokdo-Regular.ttf")]
+[assembly: ExportFont("fa-regular-400.ttf")]
+
+namespace CustomFontDemo
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CustomFontPage : ContentPage
+    {
+        public CustomFontPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
